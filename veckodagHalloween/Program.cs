f869d3a068f7db3706c9237d08de0e238b325c26
Martin Sandung <Martin Sandung@SANDUNG>
2022-09-07 12:09:37 +0200

    Add project files.

---
+++ veckodagHalloween/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace veckodagHalloween
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HalloweenWeeday();
+        }
+
+        static public void HalloweenWeeday()
+        {
+            var halloween = new DateTime(DateTime.Now.Year, 10, 31).DayOfWeek;
+            Console.WriteLine(halloween);
+        }
+    }
+}
